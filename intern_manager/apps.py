117ebe10779b28ad8a34ce9a861c879b6586558c
shresthaasim43 <105100773+shresthaasim43@users.noreply.github.com>
2022-05-28 22:22:47 +0545

    signals.py added

--- intern_manager/apps.py
+++ intern_manager/apps.py
@@ -1,6 +1,10 @@
 from django.apps import AppConfig
+from django.db.models.signals import post_save
 
 
 class InternManagerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'intern_manager'
+
+    def ready(self):
+        import intern_manager.signals
--- intern_manager/models.py
+++ intern_manager/models.py
@@ -3,8 +3,9 @@ from django.contrib.auth.models import Group, User
 
 # Create your models here.
 class UserProfile(models.Model):
-    name = models.CharField(max_length=100)
-    role = models.ForeignKey(Group, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    role = models.ForeignKey(Group, on_delete=models.CASCADE, blank=True, null=True)
 
 class Task(models.Model):
     name = models.CharField(max_length=100)
